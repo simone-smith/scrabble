fdf4034f7fa19f477032689d0480813ce52bd5a6
Simone Smith <simonecaplin@gmail.com>
2018-11-29 17:53:06 +0000

    findValidWords method checks hardcoded list for a match

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -4,4 +4,12 @@ object Game extends App {
     val rack = new Bag().generateBag().take(7)
     Player(rack)
   }
+
+  def findValidWords(rack: String): List[String] = {
+    val validWords = Set("hello", "help", "kelp")
+    Iterator.range(1, rack.length + 1)
+      .flatMap(rack.combinations)
+      .flatMap(_.permutations)
+      .filter(validWords).toList
+  }
 }
--- src/test/scala/GameSpec.scala
+++ src/test/scala/GameSpec.scala
@@ -4,4 +4,8 @@ class GameSpec extends FlatSpec with Matchers {
   "A player" should "have a rack of 7 letters" in {
     assert(Game.startGame.rack.length == 7)
   }
+
+  "A rack with the letters 'jkepils" should "return 'kelp'" in {
+    assert(Game.findValidWords("jkepils") == List("kelp"))
+  }
 }
